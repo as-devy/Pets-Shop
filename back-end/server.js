24063663d914db6e0ad299a2231c3664b51b3796
as-devy <elbedwehykhaled@gmail.com>
2025-02-08 01:54:58 +0200

    updates

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,21 @@
-const express=require("express")
-const app=express()
+const express = require("express")
+const app = express()
+const cors = require('cors');
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json()); // Parse JSON request bodies
+app.use(cors()); // Parse JSON request bodies
 
 
-app.get("/",(request,response)=>{ 
+app.get("/", (request, response) => {
     response.send("hello world")
 })
 
-app.listen(400,()=>{
+app.post("/addUser", (request, response) => {
+    const user = request.body;
+    console.log(user)
+})
+
+app.listen(400, () => {
     console.log("server is listening")
 })
