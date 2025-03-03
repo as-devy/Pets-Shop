ae5d519b51da39c8c2d850ea6bd95a9dcefa0df6
Janaj06 <130187397+Janaj06@users.noreply.github.com>
2025-03-03 01:44:37 +0000

    mmmm

--- front-end/main.js
+++ front-end/main.js
@@ -1,44 +1,4 @@
-// const signInBtn = document.querySelector("#signin")
-// const logInBtn = document.querySelector("#login")
 
-// if (signInBtn || logInBtn) {
-//     document.querySelector("#signin, #login").addEventListener("click", function (event) {
-//         event.preventDefault();
-
-//         let name = document.getElementById("name");
-//         let email = document.getElementById("email");
-//         let password = document.getElementById("password");
-//         let nameError = name.nextElementSibling;
-//         let emailError = email.nextElementSibling;
-//         let passwordError = password.nextElementSibling;
-
-//         let isValid = true;
-
-//         nameError.textContent = "";
-//         emailError.textContent = "";
-//         passwordError.textContent = "";
-
-//         if (name.value.trim() === "") {
-//             nameError.textContent = "Please Fill Out This Field.";
-//             isValid = false;
-//         }
-
-//         if (email.value.trim() === "") {
-//             emailError.textContent = "Please Fill Out This Field.";
-//             isValid = false;
-//         } else {
-//             let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-//             if (!emailPattern.test(email.value.trim())) {
-//                 emailError.textContent = "Please Enter Valid Email.";
-//                 isValid = false;
-//             }
-//         }
-//         if (password.value.trim() === "") {
-//             passwordError.textContent = "Please Fill Out This Field.";
-//             isValid = false;
-//         }
-//     });
-// }
 
 function getCookie(name) {
     const cookies = document.cookie.split('; ');
@@ -62,6 +22,11 @@ if (user) {
 document.querySelector("header .logout").addEventListener("click", ()=>{
     document.cookie = "UserId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.location.href = "./login.html"
+})
+
+
+document.querySelector("#notif_btn").addEventListener("click", ()=>{
+    document.querySelector("header .notif_list").classList.toggle("active")
 })
 
 // const createOdometer =(el, value)=>{
